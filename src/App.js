81c8ffd299d768b227d0a02f5f0ae1e5ce96d5f1
janmenjaya <janmenjayabiswal@gmail.com>
2024-03-18 14:37:19 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import "./components/videoSection/videosec.css";
 import VideoSection from "./components/videoSection/VideoSection";
-
+import ShortsSection from "./components/shorts/ShortsSection";
+import Sidenavbar from "./components/Sidenavbar";
+import Drawer from "@mui/material/Drawer";
+import { useState } from "react";
+// import IconSection from "./components/videoSection/IconSection";
 function App() {
+  const [open, setOpen] = useState(true);
+  const toggleDrawer = (newOpen) => {
+    setOpen(newOpen);
+  };
   return (
     <>
-      <Navbar />
+      <Navbar toggleDrawer={toggleDrawer} />
+      <Drawer open={open} onClose={() => toggleDrawer(false)}>
+        <Sidenavbar toggleDrawer={toggleDrawer} open={open} />
+      </Drawer>
+      {/* <IconSection /> */}
       <VideoSection />
+      <ShortsSection />
     </>
   );
 }
